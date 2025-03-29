26c5cd2bd77860bda0853e71faca36edff88344d
Lapcuatui <macone-cw38zm@192.168.1.4>
2025-03-29 14:04:50 +0700

    service handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -2,15 +2,17 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/boardService'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body', req.body)
-    console.log('req.query', req.query)
-    console.log('req.params', req.params)
+    // console.log('req.body', req.body)
+    // console.log('req.query', req.query)
+    // console.log('req.params', req.params)
+    const createdBoard = await boardService.createNew(req.body)
 
     //throw new ApiError(StatusCodes.BAD_GATEWAY, 'Toandev test error')
-    res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: APIs creat new board' })
+    res.status(StatusCodes.CREATED).json(createdBoard)
 
   } catch (error) {next(error)
     // res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
